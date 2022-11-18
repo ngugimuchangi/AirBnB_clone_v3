526526349e8f3a1acade8f75f8f1ec00592bcc42
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-18 21:39:26 +0300

    retrive number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return status OK on route /status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def obj_stats():
+    """return count of objects by type in JSON format"""
+    objects = {}
+    a_dict = {
+        'Amenity':  'amenities',
+        'City':  'cities',
+        'Place':  'places',
+        'Review':  'reviews',
+        'User':  'users',
+        'State':  'states'
+    }
+    for key, val in a_dict.items():
+        objects[val] = storage.count(key)
+    return jsonify(objects)
