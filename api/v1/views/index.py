7467933a926b350c8a8cb27cd8cdea449cdb9dfc
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-18 22:37:00 +0300

    untracked files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,8 @@
 """ views functionality
 """
 from api.v1.views import app_views
-<<<<<<< HEAD
 from flask import jsonify
 from models import storage
-=======
 from flask import jsonify, make_response
 from models import storage
 from models.amenity import Amenity
@@ -14,33 +12,12 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
->>>>>>> f893bb456cc14a164d157732f9faebe1052b4baa
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return status OK on route /status"""
-<<<<<<< HEAD
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/api/v1/stats', methods=['GET'])
-def obj_stats():
-    """return count of objects by type in JSON format"""
-    objects = {}
-    a_dict = {
-        'Amenity':  'amenities',
-        'City':  'cities',
-        'Place':  'places',
-        'Review':  'reviews',
-        'User':  'users',
-        'State':  'states'
-    }
-    for key, val in a_dict.items():
-        objects[val] = storage.count(key)
-    return jsonify(objects)
-=======
-    return make_response(jsonify({"status": "OK"}))
 
 
 @app_views.route('/stats', methods=['GET'])
@@ -51,4 +28,3 @@ def stats():
 
     obj_count = {key: storage.count(value) for key, value in classes.items()}
     return make_response(jsonify(obj_count))
->>>>>>> f893bb456cc14a164d157732f9faebe1052b4baa
