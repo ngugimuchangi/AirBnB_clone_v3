51db8a950e3a7d7479244b137ab2798fcd07bb83
ngugimuchangi <tech.curious14@gmail.com>
2022-11-19 18:09:42 +0000

    implemented place search endpoint

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,8 +5,10 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request, make_response
 from models import storage
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
+from models.state import State
 from models.user import User
 
 
@@ -68,6 +70,67 @@ def place_by_id(place_id):
                 setattr(place, key, value)
         place.save()
         return make_response(jsonify(place.to_dict()), 200)
+
+
+@app_views.route('/places_search', strict_slashes=False,
+                 methods=['POST'])
+def place_search():
+    """search places using JSON
+       data passed in the request
+       body:
+       * inclusive filter(s)
+        1. states
+        2. cities
+       * exclusive filter(s)
+        1. amenities
+    """
+    filters = get_json(request)
+    if filters is None:
+        return make_response('Not a JSON\n', 400)
+
+    places = []
+    if "states" in filters.keys():
+        states = [storage.get(State, state_id)
+                  for state_id in filters.get('states')]
+        for state in states:
+            for city in state.cities:
+                create_place_list(places, city, filters.get('amenities'))
+    if "cities" in filters.keys():
+        cities = [storage.get(City, city_id)
+                  for city_id in filters.get('cities')]
+        for city in cities:
+            create_place_list(places, city, filters.get('amenities'))
+    return make_response(jsonify(places))
+
+
+def create_place_list(place_list, city, amenities=None):
+    """ create a list of places based on specified amenities
+    """
+    if amenities is not None:
+        amenities = [storage.get(Amenity, amenity_id)
+                     for amenity_id in amenities]
+        for place in city.places:
+            place_dict = place.to_dict()
+
+            #  for db storage: query place.amenities
+            #  add attributes amenites with list of
+            #  unserializable amenities objects
+            #  the key-value pair must be removed
+            #  to prevent JSON serializaiton errors.
+            #  This also ensures consistency in the
+            #  object string representation
+            if place_dict.get("amenities"):
+                place_dict.pop("amenities")
+            if all(amenity in place.amenities for amenity in amenities):
+                if place_dict not in place_list:
+                    place_list.append(place_dict)
+    else:
+        for place in city.places:
+            place_dict = place.to_dict()
+            if place_dict.get("amenities"):
+                place_dict.pop("amenities")
+            if place.to_dict() not in place_list:
+                place_list.append(place_dict)
 
 
 def get_json(request):
