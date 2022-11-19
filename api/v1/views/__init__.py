864f913bc3d5b8acff13e0b0bea0560f62ce1ad9
ngugimuchangi <tech.curious14@gmail.com>
2022-11-19 18:35:56 +0000

    fixed pep8 errors

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,5 @@ from flask import Blueprint
 app_views = Blueprint('views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-from api.v1.views import amenities, cities, places, places_amenities, places_reviews, states, users
+from api.v1.views import (amenities, cities, places, places_amenities,
+                          places_reviews, states, users)
